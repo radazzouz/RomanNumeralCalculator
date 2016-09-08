35378cd59f2613ec39a4b61c1dd9e33bf1407ad4
Christopher Stott <christopherstott@gmail.com>
2016-09-08 11:49:59 -0700

    Initial

--- RomanNumeralCalculator/ViewController.swift
+++ RomanNumeralCalculator/ViewController.swift
@@ -20,6 +20,73 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    var total: Int = 0
+    var signOfNextOperation: Int = 1;
+
+    func update() {
+        //resultLabel.text = String(total)
+        resultLabel.text = toRoman(total)
+    }
+
+
+    func handleButton(value: Int) {
+        total += value * signOfNextOperation
+        update()
+    }
+
+    @IBOutlet weak var buttonI: UIButton!
+    @IBOutlet weak var buttonV: UIButton!
+    @IBOutlet weak var buttonX: UIButton!
+    @IBOutlet weak var buttonL: UIButton!
+    @IBOutlet weak var buttonC: UIButton!
+    @IBOutlet weak var buttonD: UIButton!
+    @IBOutlet weak var buttonM: UIButton!
+    @IBOutlet weak var buttonPlus: UIButton!
+    @IBOutlet weak var buttonMinus: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var buttonClear: UIButton!
+
+    @IBAction func actionI(sender: AnyObject) {
+        handleButton(1)
+    }
+
+    @IBAction func actionV(sender: AnyObject) {
+        handleButton(5)
+    }
+
+    @IBAction func actionX(sender: AnyObject) {
+        handleButton(10)
+    }
+
+    @IBAction func actionL(sender: AnyObject) {
+        handleButton(50)
+    }
+
+
+    @IBAction func actionC(sender: AnyObject) {
+        handleButton(100)
+    }
+
+    @IBAction func actionD(sender: AnyObject) {
+        handleButton(500)
+    }
+
+    @IBAction func actionM(sender: AnyObject) {
+        handleButton(1000)
+    }
+
+    @IBAction func actionPlus(sender: AnyObject) {
+        signOfNextOperation = 1
+    }
+
+    @IBAction func actionMinus(sender: AnyObject) {
+        signOfNextOperation = -1
+    }
+
+    @IBAction func actionClear(sender: AnyObject) {
+        total = 0
+        update()
+    }
 
 }
 
--- RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
+++ RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
@@ -22,15 +22,8 @@ class RomanNumeralCalculatorTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(toRoman(1), "I")
+        XCTAssertEqual(toRoman(2), "II")
+        XCTAssertEqual(toRoman(4), "IV")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
