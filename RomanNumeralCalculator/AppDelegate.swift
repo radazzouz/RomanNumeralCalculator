5cd859f148a1be8bbddcb9bf1091bde6f1f78418
Rad Azzouz <rad@buddybuild.com>
2017-01-18 10:17:44 -0500

    Revert "Crashing at launch"
    This reverts commit 90120dba8753f477e46a20387a86725823089570.

--- RomanNumeralCalculator/AppDelegate.swift
+++ RomanNumeralCalculator/AppDelegate.swift
@@ -15,8 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        BuddyBuildSDK.crash()
-
         BuddyBuildSDK.setup()
         
         // Override point for customization after application launch.
