fa45dbc251a6a3147d2eed73cdb4ddf45e27ef91
Rad Azzouz <rad@buddybuild.com>
2017-01-18 10:17:39 -0500

    Revert "Crashing after setting up the SDK"
    This reverts commit d82e2699d85fa160b2864b68b01221bcd31922fa.

--- RomanNumeralCalculator/AppDelegate.swift
+++ RomanNumeralCalculator/AppDelegate.swift
@@ -15,9 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        BuddyBuildSDK.setup()
         BuddyBuildSDK.crash()
 
+        BuddyBuildSDK.setup()
+        
         // Override point for customization after application launch.
         return true
     }
