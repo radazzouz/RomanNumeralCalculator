81cfc95ede2b3f865491091d4a3d2b09b143bfe2
Rad Azzouz <rad@buddybuild.com>
2017-02-02 17:25:51 -0500

    Adding BuddyBuildSDK.log() call to test it.

--- RomanNumeralCalculator/AppDelegate.swift
+++ RomanNumeralCalculator/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         BuddyBuildSDK.setup()
-        
+        BuddyBuildSDK.log("testing new logging method")
         // Override point for customization after application launch.
         return true
     }
