96dcb0e86b91fb247dc5bbb6be15fea91f1fd602
Rad Azzouz <rad@buddybuild.com>
2017-02-09 18:21:44 -0500

    POC - Video recording for UI tests

--- RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -19,46 +19,54 @@ class RomanNumeralCalculatorUITests: XCTestCase {
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        XCUIDevice().press(.home)
+        sleep(10)
         super.tearDown()
     }
     
-    func testExample() {
+    func testOnePlusOne() {
         // Use recording to get started writing UI tests.
-        
-        
         XCUIDevice.shared().orientation = .portrait
         
         let app = XCUIApplication()
+        XCUIDevice.shared().orientation = .portrait
+        XCUIApplication().buttons["Clear"].tap()
+        
         let iButton = app.buttons["I"]
         iButton.tap()
         app.buttons["+"].tap()
         iButton.tap()
 
-
-        //app.b
-        //XCTAssert(app.labels["calculated_value"].title == "II")
-
-        print(app.debugDescription)
-        print("value = " + (app.staticTexts["calculated_value"].value as? String)!)
-
         let label = app.staticTexts["calculated_value"]
         let exists = NSPredicate(format: "label == 'II'")
 
-
-
         expectation(for: exists, evaluatedWith: label, handler: nil)
         waitForExpectations(timeout: 30, handler: nil)
-
-        print(app.debugDescription)
-        print("label title = " + (label.value as! String));
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+   }
+    func testOnePlusOnePlusOne() {
+        // Use recording to get started writing UI tests.
+        XCUIDevice.shared().orientation = .portrait
+        
+        let app = XCUIApplication()
+        XCUIDevice.shared().orientation = .portrait
+        XCUIApplication().buttons["Clear"].tap()
+        
+        let iButton = app.buttons["I"]
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        
+//        let label = app.staticTexts["calculated_value"]
+//        let exists = NSPredicate(format: "label == 'III'")
+//        
+//        expectation(for: exists, evaluatedWith: label, handler: nil)
+//        waitForExpectations(timeout: 30, handler: nil)
     }
-    
 }
