a773911e73c2fe6e4a538d3bd363c6cf86997259
Christopher Stott <christopherstott@gmail.com>
2017-03-27 18:39:54 +0200

    new test

--- RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -9,12 +9,12 @@
 import XCTest
 
 class RomanNumeralCalculatorUITests: XCTestCase {
-        
+
     override func setUp() {
         super.setUp()
-        
+
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
+
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
@@ -22,18 +22,18 @@ class RomanNumeralCalculatorUITests: XCTestCase {
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
         // Use recording to get started writing UI tests.
-        
-        
+
+
         XCUIDevice.sharedDevice().orientation = .Portrait
-        
+
         let app = XCUIApplication()
         let iButton = app.buttons["I"]
         iButton.tap()
@@ -60,5 +60,35 @@ class RomanNumeralCalculatorUITests: XCTestCase {
 
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
+    func testExample2() {
+        // Use recording to get started writing UI tests.
+
+
+        XCUIDevice.sharedDevice().orientation = .Portrait
+
+        let app = XCUIApplication()
+        let iButton = app.buttons["I"]
+        iButton.tap()
+        app.buttons["+"].tap()
+        let dButton = app.buttons["D"]
+        dButton.tap()
+
+        print(app.debugDescription)
+        print("value = " + (app.staticTexts["calculated_value"].value as? String)!)
+
+        let label = app.staticTexts["calculated_value"]
+        let exists = NSPredicate(format: "label == 'II'")
+
+
+
+        expectationForPredicate(exists, evaluatedWithObject: label, handler: nil)
+        waitForExpectationsWithTimeout(30, handler: nil)
+
+        print(app.debugDescription)
+        print("label title = " + (label.value as! String));
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
 }
