ad0a4b6f72eecf07dff87d2c1d441befdeb75e9f
Christopher Stott <christopherstott@gmail.com>
2017-03-27 17:53:04 +0200

    Simple break

--- RomanNumeralCalculator/AppDelegate.swift
+++ RomanNumeralCalculator/AppDelegate.swift
@@ -16,9 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         BuddyBuildSDK.setup()
-        
+
         // Override point for customization after application launch.
-        return true
+        return tru
     }
 
     func applicationWillResignActive(application: UIApplication) {
@@ -45,4 +45,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
