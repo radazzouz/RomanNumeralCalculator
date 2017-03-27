d4f332a97e1c3afa30a99056d5cd2487f26e82ba
Christopher Stott <christopherstott@gmail.com>
2017-03-27 18:20:39 +0200

    Failing unit test

--- RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
+++ RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
@@ -10,20 +10,20 @@ import XCTest
 @testable import RomanNumeralCalculator
 
 class RomanNumeralCalculatorTests: XCTestCase {
-    
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
         XCTAssertEqual(toRoman(1), "I")
         XCTAssertEqual(toRoman(2), "II")
-        XCTAssertEqual(toRoman(4), "IV")
+        XCTAssertEqual(toRoman(4), "III")
     }
 }
