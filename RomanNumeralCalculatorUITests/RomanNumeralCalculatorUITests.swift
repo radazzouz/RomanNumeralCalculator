2f843f6211136e8b18f547b13555781e2af3c202
Rad Azzouz <rad@buddybuild.com>
2017-03-30 19:01:29 -0400

    Added new UI test case

--- RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -9,7 +9,7 @@
 import XCTest
 
 class RomanNumeralCalculatorUITests: XCTestCase {
-        
+    
     override func setUp() {
         super.setUp()
         
@@ -21,7 +21,7 @@ class RomanNumeralCalculatorUITests: XCTestCase {
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-
+        
         BuddyBuildSDK.stopUITests()
         super.tearDown()
     }
@@ -37,13 +37,13 @@ class RomanNumeralCalculatorUITests: XCTestCase {
         iButton.tap()
         app.buttons["+"].tap()
         iButton.tap()
-
+        
         let label = app.staticTexts["calculated_value"]
         let exists = NSPredicate(format: "label == 'II'")
-
+        
         expectation(for: exists, evaluatedWith: label, handler: nil)
         waitForExpectations(timeout: 30, handler: nil)
-   }
+    }
     func testOnePlusOnePlusOne() {
         // Use recording to get started writing UI tests.
         XCUIDevice.shared().orientation = .portrait
@@ -60,6 +60,60 @@ class RomanNumeralCalculatorUITests: XCTestCase {
         
         let label = app.staticTexts["calculated_value"]
         let exists = NSPredicate(format: "label == 'III'")
+        
+        expectation(for: exists, evaluatedWith: label, handler: nil)
+        waitForExpectations(timeout: 30, handler: nil)
+    }
+    func testOnePlusOneTimesTwenty() {
+        // Use recording to get started writing UI tests.
+        XCUIDevice.shared().orientation = .portrait
+        
+        let app = XCUIApplication()
+        app.buttons["Clear"].tap()
+        
+        let iButton = app.buttons["I"]
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        app.buttons["+"].tap()
+        iButton.tap()
+        
+        let label = app.staticTexts["calculated_value"]
+        let exists = NSPredicate(format: "label == 'XX'")
         
         expectation(for: exists, evaluatedWith: label, handler: nil)
         waitForExpectations(timeout: 30, handler: nil)
