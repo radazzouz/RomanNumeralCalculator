d0d9ba9b830493c465240719e1ae3072e20f387d
Rad Azzouz <rad@buddybuild.com>
2017-04-03 15:30:17 -0400

    Renamed unit tests methods too.

--- RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
+++ RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
@@ -14,12 +14,12 @@ class RomanNumeralCalculatorTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-		BuddyBuildSDK.startUITestsVideoRecording()
+		BuddyBuildSDK.startUITests()
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-		BuddyBuildSDK.stopUITestsVideoRecording()
+		BuddyBuildSDK.stopUITests()
         super.tearDown()
     }
     
