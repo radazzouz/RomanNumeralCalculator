3c0df5436c3ab19a7cbc2377e465b086927a657a
Rad Azzouz <rad@buddybuild.com>
2017-04-17 08:25:35 -0400

    Commented out the UI tests

--- RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -10,112 +10,112 @@ import XCTest
 
 class RomanNumeralCalculatorUITests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        
-        // It is important to set continueAfterFailure to true. Otherwise, the video will be properly generated, as the app get SIGKILL'd if a test fails
-        continueAfterFailure = false
-        XCUIApplication().launch()
-        BuddyBuildSDK.startUITests()
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        
-        BuddyBuildSDK.stopUITests()
-        super.tearDown()
-    }
-    
-    func testOnePlusOne() {
-        // Use recording to get started writing UI tests.
-        XCUIDevice.shared().orientation = .portrait
-        
-        let app = XCUIApplication()
-        app.buttons["Clear"].tap()
-        
-        let iButton = app.buttons["I"]
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        
-        let label = app.staticTexts["calculated_value"]
-        let exists = NSPredicate(format: "label == 'II'")
-        
-        expectation(for: exists, evaluatedWith: label, handler: nil)
-        waitForExpectations(timeout: 30, handler: nil)
-    }
-    func testOnePlusOnePlusOne() {
-        // Use recording to get started writing UI tests.
-        XCUIDevice.shared().orientation = .portrait
-        
-        let app = XCUIApplication()
-        app.buttons["Clear"].tap()
-        
-        let iButton = app.buttons["I"]
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        
-        let label = app.staticTexts["calculated_value"]
-        let exists = NSPredicate(format: "label == 'III'")
-        
-        expectation(for: exists, evaluatedWith: label, handler: nil)
-        waitForExpectations(timeout: 30, handler: nil)
-    }
-    func testOnePlusOneTimesTwenty() {
-        // Use recording to get started writing UI tests.
-        XCUIDevice.shared().orientation = .portrait
-        
-        let app = XCUIApplication()
-        app.buttons["Clear"].tap()
-        
-        let iButton = app.buttons["I"]
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        app.buttons["+"].tap()
-        iButton.tap()
-        
-        let label = app.staticTexts["calculated_value"]
-        let exists = NSPredicate(format: "label == 'XX'")
-        
-        expectation(for: exists, evaluatedWith: label, handler: nil)
-        waitForExpectations(timeout: 30, handler: nil)
-    }
+//    override func setUp() {
+//        super.setUp()
+//        
+//        // It is important to set continueAfterFailure to true. Otherwise, the video will be properly generated, as the app get SIGKILL'd if a test fails
+//        continueAfterFailure = false
+//        XCUIApplication().launch()
+//        BuddyBuildSDK.startUITests()
+//    }
+//    
+//    override func tearDown() {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//        
+//        BuddyBuildSDK.stopUITests()
+//        super.tearDown()
+//    }
+//    
+//    func testOnePlusOne() {
+//        // Use recording to get started writing UI tests.
+//        XCUIDevice.shared().orientation = .portrait
+//        
+//        let app = XCUIApplication()
+//        app.buttons["Clear"].tap()
+//        
+//        let iButton = app.buttons["I"]
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        
+//        let label = app.staticTexts["calculated_value"]
+//        let exists = NSPredicate(format: "label == 'II'")
+//        
+//        expectation(for: exists, evaluatedWith: label, handler: nil)
+//        waitForExpectations(timeout: 30, handler: nil)
+//    }
+//    func testOnePlusOnePlusOne() {
+//        // Use recording to get started writing UI tests.
+//        XCUIDevice.shared().orientation = .portrait
+//        
+//        let app = XCUIApplication()
+//        app.buttons["Clear"].tap()
+//        
+//        let iButton = app.buttons["I"]
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        
+//        let label = app.staticTexts["calculated_value"]
+//        let exists = NSPredicate(format: "label == 'III'")
+//        
+//        expectation(for: exists, evaluatedWith: label, handler: nil)
+//        waitForExpectations(timeout: 30, handler: nil)
+//    }
+//    func testOnePlusOneTimesTwenty() {
+//        // Use recording to get started writing UI tests.
+//        XCUIDevice.shared().orientation = .portrait
+//        
+//        let app = XCUIApplication()
+//        app.buttons["Clear"].tap()
+//        
+//        let iButton = app.buttons["I"]
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        app.buttons["+"].tap()
+//        iButton.tap()
+//        
+//        let label = app.staticTexts["calculated_value"]
+//        let exists = NSPredicate(format: "label == 'XX'")
+//        
+//        expectation(for: exists, evaluatedWith: label, handler: nil)
+//        waitForExpectations(timeout: 30, handler: nil)
+//    }
 }
