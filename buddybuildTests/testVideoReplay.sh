ee51143a9f24341310be35b77030a5933928a7fe
Rad Azzouz <rad@buddybuild.com>
2017-04-18 13:39:32 -0400

    Updated test script to find the video files

--- buddybuildTests/testVideoReplay.sh
+++ buddybuildTests/testVideoReplay.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-VIDEO_REPLAYS=$(find ~/Library/Developer/CoreSimulator/Devices -type f -name 'screencap-*.mp4')
+cd /tmp/sandbox/$BUDDYBUILD_BUILD_ID
+VIDEO_REPLAYS=$(find . '*.mp4')
 if [$VIDEO_REPLAYS == ""]; then
    echo "Test failed: could not find any Video Replays."
    exit -1;
